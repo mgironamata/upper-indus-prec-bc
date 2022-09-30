e376f388eeba81316f5a192a7590a8c14377f17e
marc <mgirona29@gmail.com>
2022-09-30 11:33:32 +0200

    network params to double type

--- utils.py
+++ utils.py
@@ -1133,6 +1133,7 @@ def multirun(data, predictors, params, epochs, split_by='station', sequential_sa
             raise ValueError('No valid model specified')
 
         network.to(device)
+        network.double()
 
         if model_type == "MLP":
             train_loader = DataLoader(dataset=train_dataset, batch_size=run.batch_size, shuffle=True)
