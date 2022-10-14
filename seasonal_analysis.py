f6359ba314466bfa5d5157c6876f21d71969be92
marc <mgirona29@gmail.com>
2022-10-14 21:29:33 +0100

    update seasonal_analayis.py

--- seasonal_analysis.py
+++ seasonal_analysis.py
@@ -47,104 +47,108 @@ class SeasonalAnalysis:
         return self.df.groupby(self.group_by_fields).sum()[(self.columns + self.sample_columns + self.additional_columns)].copy()
 
     def _aggregate_precipitation_occurrence_predicitons(self) -> pd.DataFrame:
-        return self.df.groupby(self.group_by_fields)[(self.columns + self.sample_columns + self.additional_columns)].agg([count_zeros]).droplevel(level=1, axis=1) # Crate dataframe for precipitation occurrence
+        return self.df.groupby(self.group_by_fields)[(self.columns + self.sample_columns + self.additional_columns)].agg([count_zeros]).droplevel(level=1, axis=1).copy() # Crate dataframe for precipitation occurrence
         
     def _absolute_error_dry_days(self) -> pd.DataFrame:
         
         keep_columns = []
 
+        for idx in self.df_agg_dry_days.index.names: keep_columns.append(idx)
+
         # DRY DAYS - ABSOLUTE ERROR 
         for i in range(self.n_samples):
-            self.df[f'edd_mlp_{i}'] = absolute_error(self.df[f'sample_{i}'], self.df['Prec'])
+            self.df_agg_dry_days[f'edd_mlp_{i}'] = absolute_error(self.df_agg_dry_days[f'sample_{i}'], self.df_agg_dry_days['Prec'])
             keep_columns.append(f'edd_mlp_{i}')
 
-        self.df['edd_mlp'] = self.df[[f'edd_mlp_{i}' for i in range(self.n_samples)]].mean(axis=1)
+        self.df_agg_dry_days['edd_mlp'] = self.df_agg_dry_days[[f'edd_mlp_{i}' for i in range(self.n_samples)]].mean(axis=1)
         keep_columns.append('edd_mlp')
         
         for c in self.columns:
-            self.df[f'edd_{c}'] = abs(self.df[c] - self.df['Prec'])
+            self.df_agg_dry_days[f'edd_{c}'] = abs(self.df_agg_dry_days[c] - self.df_agg_dry_days['Prec'])
             keep_columns.append(f'edd_{c}')
 
-        return self.df[keep_columns]
+        self.df_agg_dry_days = self.df_agg_dry_days.reset_index()
+
+        return self.df_agg_dry_days[keep_columns]
 
     def _seasonal_analysis(self) -> pd.DataFrame: 
         """Groups results by station, season and year and computes various assessment metrics."""
 
         # THIS FUNCTION IS NOT PART OF A CLASS HOWEVER MOST OBJECTS ARE CREATED NOT AS OUTSIDE PROPERTIES BUT OF CLASS. REFACTOR. 
 
-        df = self._aggregate_precipitation_intensity_predictions() # group by station, season and year (default)
+        self.df_agg = self._aggregate_precipitation_intensity_predictions() # group by station, season and year (default)
         
-        df_dry_days = self._aggregate_precipitation_occurrence_predicitons()
+        self.df_agg_dry_days = self._aggregate_precipitation_occurrence_predicitons()
 
         # df = st_test.groupby(['Station','season','year']).sum()[(columns + sample_columns + additional_columns)].copy()
 
-        df_dry_days = self._absolute_error_dry_days()
+        self.df_agg_dry_days = self._absolute_error_dry_days()
         
         # PRECIPITATION - SQUARED ERROR 
         for c in self.columns:
-            df[f'se_{c}'] = squared_error(df[c], df['Prec'])
+            self.df_agg[f'se_{c}'] = squared_error(self.df_agg[c], self.df_agg['Prec'])
             
     #     df['se_wrf_prcp'] = (df['wrf_prcp'] - df['Prec'])**2
     #     df['se_wrf_bc_prcp'] = (df['wrf_bc_prcp'] - df['Prec'])**2
 
-        df['sample'] = df[self.sample_columns].mean(axis=1)
+        self.df_agg['sample'] = self.df_agg[self.sample_columns].mean(axis=1)
 
         for i in range(self.n_samples):
-            df[f'se_mlp_{i}'] = squared_error(df[f'sample_{i}'], df['Prec'])**2
+            self.df_agg[f'se_mlp_{i}'] = squared_error(self.df_agg[f'sample_{i}'], self.df_agg['Prec'])**2
 
-        df['se_mlp'] = mean_of_samples(df, 'se_mlp', self.n_samples)
+        self.df_agg['se_mlp'] = mean_of_samples(self.df_agg, 'se_mlp', self.n_samples)
 
         # PRECIPITATION - ERROR & ABSOLUTE ERROR
         for c in self.columns:
-            df[f'e_{c}'] = error(df[c], df['Prec'])
-            df[f'ae_{c}'] = absolute_error(df[c], df['Prec'])
+            self.df_agg[f'e_{c}'] = error(self.df_agg[c], self.df_agg['Prec'])
+            self.df_agg[f'ae_{c}'] = absolute_error(self.df_agg[c], self.df_agg['Prec'])
         
-    #     df['e_wrf_prcp'] = (df['wrf_prcp'] - df['Prec'])
-    #     df['e_wrf_bc_prcp'] = (df['wrf_bc_prcp'] - df['Prec'])  
+    #     self.df_agg['e_wrf_prcp'] = (self.df_agg['wrf_prcp'] - self.df_agg['Prec'])
+    #     self.df_agg['e_wrf_bc_prcp'] = (self.df_agg['wrf_bc_prcp'] - self.df_agg['Prec'])  
 
-    #     df['ae_wrf_prcp'] = abs(df['wrf_prcp'] - df['Prec'])
-    #     df['ae_wrf_bc_prcp'] = abs(df['wrf_bc_prcp'] - df['Prec'])
+    #     self.df_agg['ae_wrf_prcp'] = abs(self.df_agg['wrf_prcp'] - self.df_agg['Prec'])
+    #     self.df_agg['ae_wrf_bc_prcp'] = abs(self.df_agg['wrf_bc_prcp'] - self.df_agg['Prec'])
 
         for i in range(self.n_samples):
-            df[f'e_mlp_{i}'] = error(df[f'sample_{i}'], df['Prec'])
-            df[f'ae_mlp_{i}'] = absolute_error(df[f'sample_{i}'], df['Prec'])
+            self.df_agg[f'e_mlp_{i}'] = error(self.df_agg[f'sample_{i}'], self.df_agg['Prec'])
+            self.df_agg[f'ae_mlp_{i}'] = absolute_error(self.df_agg[f'sample_{i}'], self.df_agg['Prec'])
 
 
-        df['e_mlp'] = mean_of_samples(df, 'e_mlp', self.n_samples)
-        df['ae_mlp'] = mean_of_samples(df, 'ae_mlp', self.n_samples)
+        self.df_agg['e_mlp'] = mean_of_samples(self.df_agg, 'e_mlp', self.n_samples)
+        self.df_agg['ae_mlp'] = mean_of_samples(self.df_agg, 'ae_mlp', self.n_samples)
 
         # PRECIPITATION - ABSOLUTE ERROR REDUCTION
         for c in self.columns:
-            df[f'aer_{c}'] = df['ae_wrf_prcp'] - df[f'ae_{c}'] 
+            self.df_agg[f'aer_{c}'] = self.df_agg['ae_wrf_prcp'] - self.df_agg[f'ae_{c}'] 
         
-        df['aer_mlp'] = df['ae_wrf_prcp'] - df['ae_mlp']
-        #df['aer_wrf_bc_prcp'] = df['ae_wrf_prcp'] - df['ae_wrf_bc_prcp']
+        self.df_agg['aer_mlp'] = self.df_agg['ae_wrf_prcp'] - self.df_agg['ae_mlp']
+        #self.df_agg['aer_wrf_bc_prcp'] = self.df_agg['ae_wrf_prcp'] - self.df_agg['ae_wrf_bc_prcp']
 
         # PRECIPITATION - MSE IMPROVEMENT RATIO
         for c in self.columns:
-            df[f'imp_{c}'] = 1 - df[f'se_{c}']/(df['se_wrf_prcp'])
+            self.df_agg[f'imp_{c}'] = 1 - self.df_agg[f'se_{c}']/(self.df_agg['se_wrf_prcp'])
         
-        #df['imp_wrf_bc_prcp'] = 1 - df['se_wrf_bc_prcp']/(df['se_wrf_prcp'])
+        #self.df_agg['imp_wrf_bc_prcp'] = 1 - self.df_agg['se_wrf_bc_prcp']/(self.df_agg['se_wrf_prcp'])
         
-        df['imp_mlp'] = 1 - df['se_mlp']/(df['se_wrf_prcp'])
+        self.df_agg['imp_mlp'] = 1 - self.df_agg['se_mlp']/(self.df_agg['se_wrf_prcp'])
 
         # PRECIPITATION - SMAPE
         for c in self.columns:
-            df[f'smape_{c}'] = df.apply(SMAPE, axis=1, args=(c, 'Prec'))
+            self.df_agg[f'smape_{c}'] = self.df_agg.apply(SMAPE, axis=1, args=(c, 'Prec'))
         
-    #     df['smape_wrf_prcp'] = df.apply(SMAPE, axis=1, args=('wrf_prcp','Prec')) 
-    #     df['smape_wrf_bc_prcp'] = df.apply(SMAPE, axis=1, args=('wrf_bc_prcp','Prec')) 
+    #     self.df_agg['smape_wrf_prcp'] = self.df_agg.apply(SMAPE, axis=1, args=('wrf_prcp','Prec')) 
+    #     self.df_agg['smape_wrf_bc_prcp'] = self.df_agg.apply(SMAPE, axis=1, args=('wrf_bc_prcp','Prec')) 
         
-        df['smape_mlp'] = df.apply(SMAPE, axis=1, args=('sample','Prec')) 
+        self.df_agg['smape_mlp'] = self.df_agg.apply(SMAPE, axis=1, args=('sample','Prec')) 
 
         if 'mean' in self.additional_columns:
-            df['smape_mlp_mean'] = df.apply(SMAPE, axis=1, args=('mean','Prec'))  
+            self.df_agg['smape_mlp_mean'] = self.df_agg.apply(SMAPE, axis=1, args=('mean','Prec'))  
         if 'median' in self.additional_columns:
-            df['smape_mlp_median'] = df.apply(SMAPE, axis=1, args=('median','Prec')) 
+            self.df_agg['smape_mlp_median'] = self.df_agg.apply(SMAPE, axis=1, args=('median','Prec')) 
 
-        df = pd.merge(df,df_dry_days,on=['Station','season','year'])
+        self.df_agg = pd.merge(self.df_agg,self.df_agg_dry_days,on=['Station','season','year'])
 
-        return df
+        return self.df_agg
 
     def seasonal_summaries(self) -> Dict[str, pd.DataFrame]:
 
@@ -194,3 +198,17 @@ class SeasonalAnalysis:
         d['smape'] = self.smape
         
         return d
+
+if __name__ == "__main__":
+
+    df = pd.read_csv("tempCSV4SA.csv")
+    columns = ['Prec','wrf_prcp','wrf_bc_prcp','precip_norris']
+    n_samples = 10
+    sample_cols = [f'sample_{i}' for i in range(n_samples)]
+    add_cols = []    
+
+    SA = SeasonalAnalysis(df, columns, sample_columns=sample_cols, additional_columns=add_cols, n_samples=n_samples)
+
+    d = SA.seasonal_summaries()
+
+    print("Done")
