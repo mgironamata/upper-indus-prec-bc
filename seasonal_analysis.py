122939c6d435c51c8d0c397db9947e49572fec48
marc <mgirona29@gmail.com>
2022-10-14 21:56:24 +0100

    update seasonal analysis

--- seasonal_analysis.py
+++ seasonal_analysis.py
@@ -1,9 +1,24 @@
 from typing import Dict, List
 import pandas as pd
+import numpy as np
 from utils import count_zeros, SMAPE, sample
+from tabulate import tabulate
+import scipy
 
-__all__ = ['SeasonalAnalysis',
-            ]
+
+"""
+TO DO:
+------
+-> INTEGREATE TABLE PLOTTING FUNCTIONS INTO SEASONAL ANALYSIS CLASS
+
+"""
+
+__all__ = [ 
+            'SeasonalAnalysis',
+            'table_of_predictions_confidence_intervals',
+            'table_of_predictions_for_metric',
+            'table_of_predictions_ks_test'
+          ]
 
 def squared_error(x : pd.Series, y : pd.Series) -> pd.Series:
     "Returns squared error between 2 pd.Series"
@@ -212,3 +227,211 @@ if __name__ == "__main__":
     d = SA.seasonal_summaries()
 
     print("Done")
+
+def table_of_predictions_for_metric(predictions, seasons, columns, n_samples, sample_columns, additional_columns, metric = 'smape', prefix='smape'):
+
+    table = []
+
+    headers = ['Model']
+    row_a = ['Bann']
+    row_b = ['BannCorr']
+    row_c = ['Norr']
+
+    for index, (k,v) in enumerate(predictions.items()):
+        
+        SA = SeasonalAnalysis(df = v['k_all'], 
+                              columns=columns, 
+                              sample_columns=sample_columns,
+                              additional_columns=additional_columns,
+                              n_samples=n_samples,
+                              group_by_fields= ['Station', 'season', 'year'])
+
+        d = SA.seasonal_summaries()
+
+        r = d[metric].copy()
+        
+        row = [f'{k}']
+        
+        for season in seasons:
+            
+            if index == 0: 
+                headers.append(f'{season} mean')
+                headers.append(f'{season} median')
+            
+            rs = r[r['season']==season].copy()
+            
+            if index==0:
+                a = rs[rs['variable']==f'{prefix}_wrf_prcp']['value']
+                b = rs[rs['variable']==f'{prefix}_wrf_bc_prcp']['value']
+                c = rs[rs['variable']==f'{prefix}_precip_norris']['value']
+                row_a.append(f'{a.mean():.2f}')
+                row_a.append(f'{a.median():.2f}')
+                row_b.append(f'{b.mean():.2f}')
+                row_b.append(f'{b.median():.2f}')
+                row_c.append(f'{c.mean():.2f}')
+                row_c.append(f'{c.median():.2f}')
+                
+            z = rs[rs['variable']==f'{prefix}_mlp']['value']
+            row.append(f'{z.mean():.2f}')
+            row.append(f'{z.median():.2f}')
+                    
+        if index==0:
+            table.append(row_a)
+            table.append(row_b)
+            table.append(row_c)
+            
+        table.append(row)
+        
+        # print(k)
+
+    print(tabulate(table, headers, tablefmt='small', disable_numparse=True))
+
+def table_of_predictions_confidence_intervals(predictions, seasons):
+    """Prints table of results. 
+
+    Previous name: table_of_results.
+    
+    Inputs:
+        predictions : dict
+    Returs:
+        None
+    
+    """
+
+    table = []
+    headers = ['Model']
+
+    for i, (k, v) in enumerate(predictions.items()):
+        if i==0:
+            row_a = ['WRF']
+            row_b = ['BC WRF']
+        row = [k]
+        st_test = v['k_all']
+        for season in seasons:
+            if i==0:
+                headers.append(f'{season} mean')
+                headers.append(f'{season} median')
+            
+            ci_lista, ci_listb = [], []
+            ci_list = []
+
+            for s in st_test['Station'].unique():
+                
+                df = st_test.loc[(st_test['Station']==s) & (st_test['season']==season)].copy()
+                
+                if i == 0:
+                    a_high = (df[f'wrf_prcp']>df['high_ci']).sum()
+                    a_low = (df[f'wrf_prcp']<df['low_ci']).sum()
+                    b_high = (df[f'wrf_prcp']>df['high_ci']).sum()
+                    b_low = (df[f'wrf_prcp']<df['low_ci']).sum()
+                    a_ci = (a_high + a_low) / len(df)
+                    b_ci = (b_high + b_low) / len(df)
+
+                    ci_lista.append(a_ci)
+                    ci_listb.append(b_ci)
+                
+                c_high = (df[f'Prec']>df['high_ci']).sum()
+                c_low = (df[f'Prec']<df['low_ci']).sum()
+                
+                c_ci = (c_high) / len(df)
+
+
+                ci_list.append(c_ci)
+
+            if i ==0:
+                row_a.append(f'{np.mean(ci_lista):.4f}')
+                row_a.append(f'{np.median(ci_lista):.4f}')
+                row_b.append(f'{np.mean(ci_listb):.4f}')
+                row_b.append(f'{np.median(ci_listb):.4f}')
+            
+            row.append(f'{np.mean(ci_list):.4f}')
+            row.append(f'{np.median(ci_list):.4f}')
+            
+        if i== 0:
+            table.append(row_a)
+            table.append(row_b)
+        
+        print(k)
+            
+        table.append(row)     
+
+    print(tabulate(table, headers, tablefmt='simple', disable_numparse=True))
+
+def table_of_predictions_ks_test(predictions, seasons, columns, sample_columns, additional_columns):
+    
+    """[description].
+    
+    Previous name: table_of_predictions
+    
+    Inputs:
+    
+    Returns:
+        None
+        
+    """
+
+    table = []
+    headers = ['Model']
+
+    for i, (k, v) in enumerate(predictions.items()):
+        if i==0:
+            row_a = ['Bann']
+            row_b = ['BannCorr']
+            row_c = ['Norr']
+            
+        row = [k]
+        st_test = v['k_all']
+        for season in seasons:
+            if i==0:
+                headers.append(f'{season} mean')
+                headers.append(f'{season} median')
+            
+            ks_lista, ks_listb, ks_listc = [], [], []
+            ks_list = []
+
+            for s in st_test['Station'].unique():
+                df = st_test[st_test['Station']==s].copy()
+                rvs = df[df['season']==season]['Prec']
+                
+                cdfa = df[df['season']==season]['wrf_prcp']
+                cdfb = df[df['season']==season]['wrf_bc_prcp']
+                cdfc = df[df['season']==season]['precip_norris']
+                
+                kstesta = scipy.stats.ks_2samp(rvs, cdfa, alternative='two-sided', mode='auto')
+                kstestb = scipy.stats.ks_2samp(rvs, cdfb, alternative='two-sided', mode='auto')
+                kstestc = scipy.stats.ks_2samp(rvs, cdfc, alternative='two-sided', mode='auto')
+                
+                ks_lista.append(kstesta)
+                ks_listb.append(kstestb)
+                ks_listc.append(kstestc)
+
+                for sample in sample_columns:
+                    cdf = np.array([])
+                    cdf_s = np.array(df[df['season']==season][sample])
+                    cdf = np.concatenate((cdf,cdf_s), axis=None)
+                    
+                kstest = scipy.stats.ks_2samp(rvs, cdf, alternative='two-sided', mode='auto')
+                ks_list.append(kstest)
+            
+            if i ==0:
+                row_a.append(f'{np.mean(ks_lista):.4f}')
+                row_a.append(f'{np.median(ks_lista):.4f}')
+                row_b.append(f'{np.mean(ks_listb):.4f}')
+                row_b.append(f'{np.median(ks_listb):.4f}')
+                row_c.append(f'{np.mean(ks_listc):.4f}')
+                row_c.append(f'{np.median(ks_listc):.4f}')
+            
+            row.append(f'{np.mean(ks_list):.4f}')
+            row.append(f'{np.median(ks_list):.4f}')
+            
+        if i== 0:
+            table.append(row_a)
+            table.append(row_b)
+            table.append(row_c)
+            
+        print(k)
+            
+        table.append(row)
+        
+    print(tabulate(table, headers, tablefmt='simple', disable_numparse=True))
+    
