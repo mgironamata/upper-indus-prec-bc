a1defbb583df110aa176940be9f5c9fc4822351c
marc <mgirona29@gmail.com>
2022-10-15 07:43:14 +0100

    move functions to new files

---
+++ likelihoods.py
@@ -0,0 +1,312 @@
+import torch
+
+from torch.distributions.gamma import Gamma
+from torch.distributions.gumbel import Gumbel
+from torch.distributions.normal import Normal
+from torch.distributions.half_normal import HalfNormal
+
+__all__ = [ 'gaussian_logpdf',
+            'gamma_logpdf',
+            'ggmm_logpdf',
+            'bgmm_logpdf',
+            'b2gmm_logpdf',
+            'b2sgmm_logpdf',
+            'bernoulli_gaussian_logpdf',
+            'bernoulli_loggaussian_logpdf',
+            'bernoulli_gumbel_logpdf',
+            'bernoulli_halfnormal_logpdf'
+          ]
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+"""Device to perform computations on."""
+
+def _reduce(logp, reduction):
+    if not reduction:
+        return logp
+    elif reduction == 'sum':
+        return torch.sum(logp)
+    elif reduction == 'mean':
+        return torch.mean(logp)
+    elif reduction == 'batched_mean':
+        return torch.mean(torch.sum(logp, 1))
+    else:
+        raise RuntimeError(f'Unknown reduction "{reduction}".')
+
+def gaussian_logpdf(obs, mu, sigma, reduction='mean'):
+    """Gamma mixture model log-density.
+
+    Args:
+        obs (torch.Tensor): Observed values.
+        alpha (torch.Tensor)): Paramaters 'alpha' from Gamma distribution.
+        beta (torch.Tensor): Pamaterers 'beta' from Gamma distribution. 
+        reduction (str, optional): Reduction. Defaults to no reduction.
+            Possible values are "sum", "mean", and "batched_mean".
+
+    Returns:
+        torch.Tensor: Log-density.
+    """
+
+    obs = obs.flatten()
+
+    logp = Normal(loc=mu, scale=sigma).log_prob(obs)
+
+    return _reduce(logp, reduction)
+
+def gamma_logpdf(obs, alpha, beta, reduction='mean'):
+    """Gamma mixture model log-density.
+
+    Args:
+        obs (torch.Tensor): Observed values.
+        alpha (torch.Tensor)): Paramaters 'alpha' from Gamma distribution.
+        beta (torch.Tensor): Pamaterers 'beta' from Gamma distribution. 
+        reduction (str, optional): Reduction. Defaults to no reduction.
+            Possible values are "sum", "mean", and "batched_mean".
+
+    Returns:
+        torch.Tensor: Log-density.
+    """
+
+    obs = obs.flatten()
+    b_mask = obs == 0
+
+    epsilon = 0.000001
+    obs[b_mask] = obs[b_mask] + epsilon
+
+    logp = Gamma(concentration=alpha, rate=beta).log_prob(obs)
+
+    return _reduce(logp, reduction)
+
+def ggmm_logpdf(obs, alpha1, alpha2, beta1, beta2, q, reduction='mean'):
+    """Benroulli-Gamma-Gamma mexture model log-density.
+
+    Args:
+        obs (torch.Tensor): Inputs.
+        alpha1 (torch.Tensor): 
+        beta1 (torch.Tensor):
+        alpha1 (torch.Tensor): 
+        beta1 (torch.Tensor):
+        q (torch.Tensor):
+        reduction (str, optional): Reduction. Defaults to no reduction.
+            Possible values are "sum", "mean", and "batched_mean".
+
+    Returns:
+        torch.Tensor: Log-density.
+    """
+
+    obs = obs.flatten()
+    b_mask = obs == 0
+
+    epsilon = 0.000001
+    obs[b_mask] = obs[b_mask] + epsilon
+
+    mixture_weights = torch.stack([q,1-q]).permute([1,0]) # REVIEW
+    mixture_alphas = torch.stack([alpha1,alpha2]).permute([1,0])
+    mixture_betas = torch.stack([beta1,beta2]).permute([1,0])
+
+    mix = torch.distributions.Categorical(mixture_weights)
+    comp = torch.distributions.Gamma(mixture_alphas, mixture_betas)
+    gmm = torch.distributions.mixture_same_family.MixtureSameFamily(mix, comp)   
+
+    logp = gmm.log_prob(obs)
+
+    return _reduce(logp, reduction)
+
+def bgmm_logpdf(obs, pi, alpha, beta, reduction='mean'):
+    """Benroulli-Gamma mixture model log-density.
+
+    Args:
+        obs (torch.Tensor): Inputs.
+        pi (torch.Tensor): 
+        alpha (torch.Tensor): 
+        beta (torch.Tensor):
+        reduction (str, optional): Reduction. Defaults to no reduction.
+            Possible values are "sum", "mean", and "batched_mean".
+
+    Returns:
+        torch.Tensor: Log-density.
+    """
+    
+    #pdb.set_trace()
+
+    obs = obs.flatten()
+    logp = torch.zeros(obs.shape, device=device)
+    
+    b_mask = obs == 0
+    g_mask = obs > 0
+
+    logp[g_mask] = torch.log((1-pi[g_mask])) + Gamma(concentration=alpha[g_mask], rate=beta[g_mask]).log_prob(obs[g_mask])
+    logp[b_mask] = torch.log(pi[b_mask])
+
+    return _reduce(logp, reduction)
+     
+def b2gmm_logpdf(obs, pi, alpha1, alpha2, beta1, beta2, q, reduction='mean'):
+    """Benroulli-Gamma-Gamma mexture model log-density.
+
+    Args:
+        obs (torch.Tensor): Inputs.
+        pi (torch.Tensor): 
+        alpha1 (torch.Tensor): 
+        beta1 (torch.Tensor):
+        alpha1 (torch.Tensor): 
+        beta1 (torch.Tensor):
+        q (torch.Tensor):
+        reduction (str, optional): Reduction. Defaults to no reduction.
+            Possible values are "sum", "mean", and "batched_mean".
+
+    Returns:
+        torch.Tensor: Log-density.
+    """
+
+    obs = obs.flatten()
+    logp = torch.zeros(obs.shape, device=device)
+    
+    b_mask = obs == 0
+    g_mask = obs > 0
+    
+    k = g_mask.shape[0]
+
+    mixture_weights = torch.stack([q[g_mask],1-q[g_mask]]).permute([1,0]) # REVIEW
+    mixture_alphas = torch.stack([alpha1[g_mask],alpha2[g_mask]]).permute([1,0])
+    mixture_betas = torch.stack([beta1[g_mask],beta2[g_mask]]).permute([1,0])
+
+    mix = torch.distributions.Categorical(mixture_weights)
+    comp = torch.distributions.Gamma(mixture_alphas, mixture_betas)
+    gmm = torch.distributions.mixture_same_family.MixtureSameFamily(mix, comp)   
+
+    logp[g_mask] = torch.log(1-pi[g_mask]) + gmm.log_prob(obs[g_mask])
+    logp[b_mask] = torch.log(pi[b_mask])
+
+    return _reduce(logp, reduction)
+
+def b2sgmm_logpdf(obs, pi, alpha1, alpha2, beta1, beta2, q, t, reduction='mean'):
+    """Benroulli-Gamma-Gamma mixture model log-density.
+
+    Args:
+        obs (torch.Tensor): Inputs.
+        pi (torch.Tensor): 
+        alpha (torch.Tensor): 
+        beta (torch.Tensor):
+        q (torch.Tensor):
+        t (torch.Tensor):
+        reduction (str, optional): Reduction. Defaults to no reduction.
+            Possible values are "sum", "mean", and "batched_mean".
+
+    Returns:
+        torch.Tensor: Log-density.
+    """
+    
+    obs = obs.flatten()
+    logp = torch.zeros(obs.shape, device=device)
+    
+    b_mask = obs == 0
+    g1_mask = (obs > 0) * (obs < t)
+    g2_mask = (obs > 0) * (obs >= t)
+
+    logp[g1_mask] = torch.log((1-pi[g1_mask])) + torch.log((q[g1_mask])) + Gamma(concentration=alpha1[g1_mask], rate=beta1[g1_mask]).log_prob(obs[g1_mask])
+    logp[g2_mask] = torch.log((1-pi[g2_mask])) + torch.log((1-q[g2_mask])) + Gamma(concentration=alpha2[g2_mask], rate=beta2[g2_mask]).log_prob(obs[g2_mask])
+    logp[b_mask] = torch.log(pi[b_mask])
+
+    return _reduce(logp, reduction)
+
+def bernoulli_gaussian_logpdf(obs, pi, mu, sigma, reduction='mean'):
+    """Benroulli-Gaussian mixture model log-density.
+
+    Args:
+        obs (torch.Tensor): Inputs.
+        pi (torch.Tensor): 
+        mu (torch.Tensor): 
+        sigma (torch.Tensor):
+        reduction (str, optional): Reduction. Defaults to no reduction.
+            Possible values are "sum", "mean", and "batched_mean".
+
+    Returns:
+        torch.Tensor: Log-density.
+    """
+
+    obs = obs.flatten()
+    logp = torch.zeros(obs.shape, device=device)
+    
+    b_mask = obs == 0
+    g_mask = obs != 0
+
+    logp[g_mask] = torch.log((1-pi[g_mask])) + Normal(loc=mu[g_mask], scale=sigma[g_mask]).log_prob(obs[g_mask])
+    logp[b_mask] = torch.log(pi[b_mask])
+
+    return _reduce(logp, reduction)
+
+def bernoulli_loggaussian_logpdf(obs, pi, mu, sigma, reduction='mean'):
+    """Benroulli-Gaussian mixture model log-density.
+
+    Args:
+        obs (torch.Tensor): Inputs.
+        pi (torch.Tensor): 
+        mu (torch.Tensor): 
+        sigma (torch.Tensor):
+        reduction (str, optional): Reduction. Defaults to no reduction.
+            Possible values are "sum", "mean", and "batched_mean".
+
+    Returns:
+        torch.Tensor: Log-density.
+    """
+
+    obs = obs.flatten()
+    logp = torch.zeros(obs.shape, device=device)
+    
+    b_mask = obs == 0
+    g_mask = obs != 0
+
+    logp[g_mask] = torch.log((1-pi[g_mask])) + Normal(loc=mu[g_mask], scale=sigma[g_mask]).log_prob(torch.log(obs[g_mask]))
+    logp[b_mask] = torch.log(pi[b_mask])
+
+    return _reduce(logp, reduction)
+
+def bernoulli_gumbel_logpdf(obs, pi, mu, beta, reduction='mean'):
+    """Benroulli-Gumbel mixture model log-density.
+
+    Args:
+        obs (torch.Tensor): Inputs.
+        pi (torch.Tensor): 
+        mu (torch.Tensor): 
+        beta (torch.Tensor):
+        reduction (str, optional): Reduction. Defaults to no reduction.
+            Possible values are "sum", "mean", and "batched_mean".
+
+    Returns:
+        torch.Tensor: Log-density.
+    """
+
+    obs = obs.flatten()
+    logp = torch.zeros(obs.shape, device=device)
+    
+    b_mask = obs == 0
+    g_mask = obs != 0
+
+    logp[g_mask] = torch.log((1-pi[g_mask])) + Gumbel(loc=mu[g_mask], scale=beta[g_mask]).log_prob(obs[g_mask])
+    logp[b_mask] = torch.log(pi[b_mask])
+
+    return _reduce(logp, reduction)
+
+def bernoulli_halfnormal_logpdf(obs, pi, sigma, reduction='mean'):
+    """Benroulli-HalfNormal mixture model log-density.
+
+    Args:
+        obs (torch.Tensor): Inputs.
+        pi (torch.Tensor): 
+        sigma (torch.Tensor): 
+        reduction (str, optional): Reduction. Defaults to no reduction.
+            Possible values are "sum", "mean", and "batched_mean".
+
+    Returns:
+        torch.Tensor: Log-density.
+    """
+
+    obs = obs.flatten()
+    logp = torch.zeros(obs.shape, device=device)
+    
+    b_mask = obs == 0
+    g_mask = obs != 0
+
+    logp[g_mask] = torch.log((1-pi[g_mask])) + HalfNormal(scale=sigma[g_mask]).log_prob(obs[g_mask])
+    logp[b_mask] = torch.log(pi[b_mask])
+
+    return _reduce(logp, reduction)
---
+++ metrics.py
@@ -0,0 +1,29 @@
+import pandas as pd
+
+__all__ = [
+            'SMAPE',
+            'BS',
+            'QS' 
+          ]
+
+def SMAPE(df : pd.Dataframe, sim : str, obs : str):
+    "Symmetrical Mean Absolute Percentage Error"
+    if abs(df[sim] - df[obs]) == 0:
+        return 0
+    else:
+        return abs(df[sim] - df[obs]) / (df[sim] + df[obs])
+
+def BS(df : pd.Dataframe, sim : str, obs : str, wet_threshold : float = 0):
+    "Brier score"
+    if df[obs] > wet_threshold:
+        return (df[sim] - 1)**2
+    else:
+        return df[sim]**2
+
+def QS(df : pd.Dataframe, sim : str, obs : str, quantile : float):
+    "Quantile Score"
+    d = df[obs] - df[sim]
+    if d < 0:
+        return (quantile-1) * d
+    else:
+        return quantile * d
