9615b5e33a6411c5a1733d3cd543d01889291e55
marc <mgirona29@gmail.com>
2022-10-15 08:20:41 +0100

    changes to metrics

--- metrics.py
+++ metrics.py
@@ -6,21 +6,21 @@ __all__ = [
             'QS' 
           ]
 
-def SMAPE(df : pd.Dataframe, sim : str, obs : str):
+def SMAPE(df : pd.DataFrame, sim : str, obs : str):
     "Symmetrical Mean Absolute Percentage Error"
     if abs(df[sim] - df[obs]) == 0:
         return 0
     else:
         return abs(df[sim] - df[obs]) / (df[sim] + df[obs])
 
-def BS(df : pd.Dataframe, sim : str, obs : str, wet_threshold : float = 0):
+def BS(df : pd.DataFrame, sim : str, obs : str, wet_threshold : float = 0):
     "Brier score"
     if df[obs] > wet_threshold:
         return (df[sim] - 1)**2
     else:
         return df[sim]**2
 
-def QS(df : pd.Dataframe, sim : str, obs : str, quantile : float):
+def QS(df : pd.DataFrame, sim : str, obs : str, quantile : float):
     "Quantile Score"
     d = df[obs] - df[sim]
     if d < 0:
