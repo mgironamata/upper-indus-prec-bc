408cfaefe90ef615da6974f0a354e4579214c0bb
marc <mgirona29@gmail.com>
2022-10-16 17:44:37 +0100

    Update CONFIG

--- CONFIG.py
+++ CONFIG.py
@@ -34,7 +34,7 @@ predictors = [
                 'X',
                 'Y',
                 #'aspect',
-                #'slope',
+                # 'slope',
                 'year',
                 'CWV_norris', 
                 'RH2_norris', 'RH500_norris', 
@@ -49,11 +49,10 @@ predictand = ['Prec']
 params = OrderedDict(
     lr = [0.005]
     ,batch_size = [128]
-    ,likelihood_fn = ['bgmm','bernoulli_loggaussian'] #, 'bernoulli_loggaussian', 'b2gmm'] #['bernoulli_loggaussian']
+    ,likelihood_fn = ['bgmm','bernoulli_loggaussian'] # 'bernoulli_loggaussian', 'b2gmm']
     ,hidden_channels = [[50]] #[[10],[30],[50],[100],[10,10],[30,30],[50,50],[100,100]]
     ,dropout_rate = [0]
-    ,linear_model = [False, True] #['True','False']
-    #,k = [0]
+    ,linear_model = [False, True]
     ,k = list(range(2))
 )
 
